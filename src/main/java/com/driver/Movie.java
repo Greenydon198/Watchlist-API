2fcd0ac1b39c0f3187fd1321772fdba8bac0287f
Greeny Don <greenydon198@gmail.com>
2023-05-23 18:02:27 +0530

    2

--- src/main/java/com/driver/Movie.java
+++ src/main/java/com/driver/Movie.java
@@ -1,14 +1,34 @@
 package com.driver;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class Movie {
     private String name;
     private int durationInMinutes;
     private double imdbRating;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    public double getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(double imdbRating) {
+        this.imdbRating = imdbRating;
+    }
 
     public Movie() {
     }
